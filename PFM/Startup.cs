46db1f2bc739f5f98d39348db4447c7cb92b8a65
Zachary Howell <milosh55555@gmail.com>
2017-11-13 16:28:12 -0500

    Project starting

--- PFM/Startup.cs
+++ PFM/Startup.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PFM.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +8,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
 namespace PFM
 {
     public class Startup
@@ -22,6 +23,8 @@ namespace PFM
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var connection = @"Server=tcp:personalfinancemanager.database.windows.net,1433;Initial Catalog=PersonalFinanceManagerDB;Persist Security Info=False;User ID=Group6;Password=Sabekaza6;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<PersonalFinanceManagerDBContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
