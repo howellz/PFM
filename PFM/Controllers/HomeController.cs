d6223d333745d8ab7c92924901a0759af3786840
jacks487 <jacks487@email.uscupstate.edu>
2017-11-30 16:01:45 -0500

    Edited Contact and About pages

--- PFM/Controllers/HomeController.cs
+++ PFM/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace PFM.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Welcome to Your Personal Finance Manager!";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Contact Information";
 
             return View();
         }
